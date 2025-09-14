d39245b615e26a31f610d7849d1c0700091e35d6
Umut <alkanumut848@gmail.com>
2025-09-14 21:11:00 +0300

    Add Error Message

--- checker.c
+++ checker.c
@@ -6,7 +6,7 @@
 /*   By: ualkan <ualkan@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/14 00:43:26 by ualkan            #+#    #+#             */
-/*   Updated: 2025/09/14 15:58:49 by ualkan           ###   ########.fr       */
+/*   Updated: 2025/09/14 21:00:30 by ualkan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,10 @@
 int	ft_checker(int ac, char **av, t_sim *f)
 {
 	if (ac != 5 && ac != 6)
+	{
+		printf("Error: Lack of Arguments\n");
 		return (-1);
+	}
 	f->philo_count = ft_atoi(av[1]);
 	f->time_to_die = ft_atoi(av[2]);
 	f->time_to_eat = ft_atoi(av[3]);
@@ -25,14 +28,16 @@ int	ft_checker(int ac, char **av, t_sim *f)
 		f->must_eat_count = ft_atoi(av[5]);
 	if (f->philo_count == -42 || f->time_to_die == -42
 		|| f->time_to_eat == -42 || f->time_to_sleep == -42
-		|| (ac == 6 && f->must_eat_count == -42))
-		return (-1);
-	if (f->philo_count < 1 || f->philo_count >= 200
+		|| (ac == 6 && f->must_eat_count == -42)
+		|| f->philo_count < 1 || f->philo_count >= 200
 		|| f->time_to_die <= 0 || f->time_to_eat <= 0 || f->time_to_sleep <= 0
 		|| f->time_to_eat >= f->time_to_die
 		|| f->time_to_sleep >= f->time_to_die
 		|| (ac == 6 && f->must_eat_count <= 0))
+	{
+		printf("Error: Invalid Arguments\n");
 		return (-1);
+	}
 	return (0);
 }
 
